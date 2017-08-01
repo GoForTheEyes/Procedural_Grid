87bc1b3d00b34888f7befa8de4353a6f1fa158fa
GoForTheEyes <brabandere@gmail.com>
2017-08-01 00:15:03 -0400

    UV test
    Tested procedural mesh generator with a UV test texture

---
+++ Assets/Grid.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Grid : MonoBehaviour {
+
+    public int xSize, ySize;
+
+
+    private Mesh mesh;
+    private Vector3[] vertices;
+
+    private void Awake()
+    {
+        Generate();
+    }
+
+    private void Generate()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
+        //iterates y and initialized i
+        for (int i = 0, y =0; y <= ySize; y++) {
+            //iterates x and y
+            for (int x = 0; x <= xSize; x++,i++) {
+                vertices[i] = new Vector3(x, y);
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+
+        //Triangles inside vertices
+        //The triangle needs the first vertex to be smallest, and the second the hightest so it it is clockwise
+        //only clockwise objects are considered "forward looking" and thus visible
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
+            for (int x = 0; x < xSize; ti += 6, x++, vi++) {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+        }
+        mesh.triangles = triangles;
+        //by calculating normals there is a light effect that provides the illusion of depth
+        mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
